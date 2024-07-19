c4521e4a44cf09d9ca04dbfdfc1ce1bed723c9cf
Manideep9931 <manideep.22bce9931@vitapstudent.ac.in>
2024-07-19 07:54:26 +0530

    Delete frontend directory

--- frontend/src/auth/helper/AdminRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Route,Navigate} from "react-router-dom"
-import {isAuthenticated} from "./index"
-
-const AdminRoute = ({ children }) => {
-    return isAuthenticated() && isAuthenticated().user.role===1 ? children : <Navigate to="/signin" />;
-};
-
-export default AdminRoute
--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AppRoutes from "./Routes"; 
-import { createRoot } from 'react-dom/client';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<AppRoutes />);
